46e494efc216ec267b3d9a00e5357757daacd035
Nick <nikmaunt@gmail.com>
2023-01-29 15:22:59 +0300

    init commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*hash router,provider*/}
+      <>
+          {/*  <Main/>*/}
+          {/*<Header/>*/}
+          {/*<Routes/>*/}
+      </>
+    </div>
+  );
+}
+
+export default App;
